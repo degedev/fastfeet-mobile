29ac8488cf2641227209edc70ac655114f902541
Icaro Ramos <polosnramos@hotmail.com>
2024-06-26 23:39:12 -0300

    feat: create theme styles and add typings

---
+++ fastfeet/src/theme/index.ts
@@ -0,0 +1,44 @@
+export default {
+  colors: {
+    primaryBlue: {
+      main: "#4C33CC",
+      dark: "#4C4766",
+    },
+    primaryYellow: {
+      main: "#FFBE00",
+      light: "#FFF1D6",
+    },
+    gray: {
+      lightest: "#F7F5FA",
+      light: "#D4CCFF",
+      dark: "#6F6C80",
+    },
+    white: "#FFFFFF",
+    primaryGreen: {
+      main: "#00DA6D",
+    },
+    primaryRed: {
+      main: "#E52E2E",
+    },
+  },
+  fonts: {
+    higlight: {
+      main: "    RobotoCondensed_700Bold",
+      alternative: "RobotoCondensed_700Bold_Italic",
+    },
+    body: {
+      regular: "Inter_400Regular",
+      medium: "Inter_500Medium",
+      bold: "Inter_700Bold",
+    },
+  },
+  fontSizes: {
+    extraSmall: 10,
+    small: 13,
+    medium: 15,
+    large: 16,
+    extraLarge: 22,
+    extraExtraLarge: 32,
+    gigantic: 48,
+  },
+};

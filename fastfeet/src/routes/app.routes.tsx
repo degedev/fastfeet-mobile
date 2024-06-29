13db223d0f97ab9b19ff6dd925dab8c5ddf7d263
Icaro Ramos <polosnramos@hotmail.com>
2024-06-28 23:29:02 -0300

    feat: add mapping for ForgotPassword screen

--- fastfeet/src/routes/app.routes.tsx
+++ fastfeet/src/routes/app.routes.tsx
@@ -1,12 +1,25 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-const { Navigator, Screen } = createNativeStackNavigator();
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
 
 import { Home } from "../screens/Home";
+import { ForgotPassword } from "../screens/ForgotPassword";
+
+type AppRoutes = {
+  home: undefined;
+  forgotPassword: undefined;
+};
+
+export type AppNavigatorRoutesProps = NativeStackNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createNativeStackNavigator<AppRoutes>();
 
 export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
+      <Screen name="forgotPassword" component={ForgotPassword} />
     </Navigator>
   );
 }
